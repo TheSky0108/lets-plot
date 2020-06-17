0d8ec38dd72b7e7b821211a09e4c7d9965137758
Valentin Dobidaytis <vdovidaytis@horis.ru>
2020-06-17 19:36:48 +0300

    Added napoleon extension.

--- docs/sphinx-doc/source/conf.py
+++ docs/sphinx-doc/source/conf.py
@@ -27,7 +27,11 @@ author = 'JetBrains'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['jupyter_sphinx']
+extensions = ['jupyter_sphinx', 'sphinx.ext.napoleon']
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_use_param = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
